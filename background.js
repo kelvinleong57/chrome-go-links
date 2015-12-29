21fd7261440d182a100a3d7d75315b5449ff20b5
Kelvin Leong <kelvin.leong@berkeley.edu>
2015-12-29 00:44:46 -0800

    omnibox can add go-links

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-// var root_url = 'http://testing.berkeley-pbl.com/'
-var root_url = 'http://localhost:5000/'
 
 function resetDefaultSuggestion() {
 	chrome.omnibox.setDefaultSuggestion({
@@ -8,26 +6,21 @@ function resetDefaultSuggestion() {
 }
 resetDefaultSuggestion();
 
-function navigate(url) {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.update(tabs[0].id, {url: url});
-	});
-}
-
 chrome.omnibox.onInputEntered.addListener(function(text) {
 	if(text.indexOf('add:') != -1){
 		key = text.split('add:')[1].trim();
+
 		chrome.tabs.getSelected(null,function(tab) {
-					url = tab.url;
-					if(key == ""){
-						key = tab.title;
-					}
-					navigate(root_url + "/go/add_landing_page?key="+encodeURIComponent(key)+"&url="+encodeURIComponent(url));
-					
+			if(key == ""){
+				key = tab.title;
+			}
+			encoded_key = encodeURIComponent(key);
+			encoded_url = encodeURIComponent(tab.url);
+
+			generate_post_request(encoded_key, encoded_url, false);
 		});
 
 	} else {
-		// navigate("http://pbl.link/" + text);
 		navigate(root_url + 'go/' + text);
 	}
 });
